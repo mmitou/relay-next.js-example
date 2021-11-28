8912ec4a2924d78b367abd9f39a9e2044e3e2ecc
mmitou <masayuki.itou.work@gmail.com>
2021-11-28 18:49:36 +0900

    add box

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,23 @@
 import {
   AppBar,
   Button,
+  Box,
   Container,
+  Drawer,
   IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
   Toolbar,
   Typography,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import { FC } from "react";
+import PersonIcon from "@mui/icons-material/Person";
+import { FC, useState } from "react";
 
 const Layout: FC = ({ children }) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   return (
     <>
       <AppBar position="static">
@@ -20,6 +28,7 @@ const Layout: FC = ({ children }) => {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={() => setIsDrawerOpen(true)}
           >
             <MenuIcon />
           </IconButton>
@@ -29,6 +38,26 @@ const Layout: FC = ({ children }) => {
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+      <Drawer
+        anchor="left"
+        open={isDrawerOpen}
+        onClose={() => setIsDrawerOpen(false)}
+      >
+        <Box
+          sx={{ width: 200 }}
+          role="presentation"
+          onClick={() => setIsDrawerOpen(false)}
+        >
+          <List>
+            <ListItem button key="user">
+              <ListItemIcon>
+                <PersonIcon />
+              </ListItemIcon>
+              <ListItemText primary="user" />
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
       <Container disableGutters={true} maxWidth="sm" component="main">
         {children}
       </Container>
