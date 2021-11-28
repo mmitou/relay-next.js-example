346dc47ccfc0c68db6f017b922fc495ff030565f
mmitou <masayuki.itou.work@gmail.com>
2021-11-28 17:02:51 +0900

    add getstaticprops

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,8 +1,19 @@
 import { NextPage } from "next";
 import UserInfo from "../src/UserInfo";
 
-const User: NextPage = () => {
-  return <UserInfo />;
+interface Props {
+  flag: boolean;
+}
+
+const User: NextPage<Props> = ({ flag }: Props) => {
+  if (flag) {
+    return <UserInfo />;
+  }
+  return <p>developing...</p>;
 };
+
+export async function getStaticProps() {
+  return { props: { flag: false } };
+}
 
 export default User;
